e767f7ed9b17e6ae8eb34cd1123e15527a6a7841
Infinity Botz <transformers5720p+18june25op@gmail.com>
2026-02-13 23:08:51 +0530

    File "/workspace/plugins/pm_filter.py", line 848

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -841,11 +841,11 @@ async def auto_filter(client, msg, spoll=False):
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
             return
         if len(message.text) < 100:
-            search = await replace_words(message.text)		
+            search = await replace_words(message.text)      
             search = search.lower()
             search = search.replace("-", " ")
-            search = search.replace(":","")
-			search = search.replace("'","")
+            search = search.replace(":", "")
+            search = search.replace("'", "")
             search = re.sub(r'\s+', ' ', search).strip()
             m=await message.reply_text(f'<b>Wait {message.from_user.mention} Searching Your Query: <i>{search}...</i></b>', reply_to_message_id=message.id)
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
